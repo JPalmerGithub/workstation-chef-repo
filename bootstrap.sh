14e3de4bd32a136001ac0a72835b91267548a473
Nick Lopez <nlopez@gmail.com>
2013-04-04 00:23:05 -0400

    updates bootstrap script, renames user to vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-readonly url_homebrew="https://raw.github.com/mxcl/homebrew/go"
+readonly url_homebrew_go="https://raw.github.com/mxcl/homebrew/go"
 readonly url_xcode_cltools="http://goo.gl/7pXDw"
 # install homebrew
-if ! hash brew 2>/dev/null; then ruby -e "$(curl -fsSL $url_homebrew)"; fi
+if ! hash brew 2>/dev/null; then ruby -e "$(curl -fsSL $url_homebrew_go)"; fi
 # install os x command line tools dmg
 if [ ! $(pkgutil --pkgs="com.apple.pkg.DeveloperToolsCLI") ]; then
   curl -L "$url_xcode_cltools" > cltools.dmg
@@ -15,13 +15,19 @@ brew doctor
 brew tap homebrew/dupes # optional
 # get a sane and recent ruby
 brew install rbenv ruby-build
+(
+cat <<'EOF'
+export RBENV_ROOT=/usr/local/var/rbenv
+export PATH=/usr/bin:/bin/:/usr/sbin:/usr/local/bin:/usr/local/sbin
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+EOF
+) >> ~/.profile
+source ~/.profile
 rbenv install 1.9.3-p392
 # ensure our bootstrap script is using our new ruby
 rbenv global 1.9.3-p392
-eval "$(rbenv init -)"
 ruby --version # sanity check
 # install chef and make it available to our script
 gem install chef --no-rdoc --no-ri
-rbenv rehash
 # you're now ready to run chef-solo!
 chef-solo -c ./solo.rb -j ./roles/workstation.json
