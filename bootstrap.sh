59114001e3688f710b66c5d34cf8017554503ead
Nick Lopez <nlopez@gmail.com>
2013-04-04 00:24:11 -0400

    install xcode clt first

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 readonly url_homebrew_go="https://raw.github.com/mxcl/homebrew/go"
 readonly url_xcode_cltools="http://goo.gl/7pXDw"
-# install homebrew
-if ! hash brew 2>/dev/null; then ruby -e "$(curl -fsSL $url_homebrew_go)"; fi
 # install os x command line tools dmg
 if [ ! $(pkgutil --pkgs="com.apple.pkg.DeveloperToolsCLI") ]; then
   curl -L "$url_xcode_cltools" > cltools.dmg
@@ -10,6 +8,8 @@ if [ ! $(pkgutil --pkgs="com.apple.pkg.DeveloperToolsCLI") ]; then
   sudo installer -package "$volume/Command Line Tools (Mountain Lion).mpkg" -target /
   hdiutil detach "$volume"
 fi
+# install homebrew
+if ! hash brew 2>/dev/null; then ruby -e "$(curl -fsSL $url_homebrew_go)"; fi
 # verify homebrew is good to go
 brew doctor
 brew tap homebrew/dupes # optional
