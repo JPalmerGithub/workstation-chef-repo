01e19fc14425eb11244f89888a076d2c1b2505d8
Nick Lopez <nlopez@gmail.com>
2013-04-05 23:51:18 -0400

    adds wireshark cookbook

---
+++ cookbooks/wireshark/metadata.rb
@@ -0,0 +1,10 @@
+name             "wireshark"
+maintainer       "Nick Lopez"
+maintainer_email "nlopez@gmail.com"
+license          "Apache 2.0"
+description      "Installs Wireshark"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+
+depends "dmg", "~> 1.1"
+depends "xquartz", "~> 1.0"
