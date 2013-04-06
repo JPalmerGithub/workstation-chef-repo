da504219b7b8ffd20ff1edca236d5b2c7299d6a6
Nick Lopez <nlopez@gmail.com>
2013-04-06 19:22:42 -0400

    adds basics for mini

---
+++ roles/mini.rb
@@ -0,0 +1,14 @@
+name 'mini'
+description 'Role applied to all Mac OS X systems.'
+run_list(
+  'recipe[workstation::mini]',
+  'recipe[mac_os_x::settings]',
+  'recipe[mac_os_x::firewall]',
+  'recipe[zip_app::data_bag]',
+  'recipe[oh-my-zsh-solo]',
+  # Roles
+  'role[mac_os_x]'
+)
+default_attributes(
+)
+
