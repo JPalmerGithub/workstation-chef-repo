03412301460665fc75b0d5f09634868f6ead16f6
Nick Lopez <nlopez@amplify.com>
2014-03-18 14:47:37 -0400

    remove rbenv

--- roles/base.rb
+++ roles/base.rb
@@ -10,33 +10,5 @@ default_attributes(
       'users' => 'nlopez',
       'passwordless' => true
     }
-  },
-  'ruby_build' => {
-    'git_ref' => 'v20130227',
-    'upgrade' => true,
-    'install_pkgs' => []
-  },
-  'rbenv' => {
-    'root_path' => '/usr/local/var/rbenv',
-    'install_pkgs' => [],
-    'user_installs' => [
-      {
-        'user' => 'nlopez',
-        'rubies' => [ '1.9.3-p392' ],
-        'global' => '1.9.3-p392',
-        'gems' => {
-          '1.9.3-p392' => [
-            {'name' => 'chef'},
-            {'name' => 'knife-spork'},
-            {'name' => 'knife-block'},
-            {'name' => 'knife-ec2'},
-            {'name' => 'knife-whisk'},
-            {'name' => 'knife-config'},
-            {'name' => 'bundler'},
-            {'name' => 'berkshelf'}
-          ]
-        }
-      }
-    ]
   }
 )
--- roles/workstation.rb
+++ roles/workstation.rb
@@ -2,21 +2,11 @@ name 'base'
 description 'Role applied to all Mac OS X systems.'
 run_list(
   'recipe[git]',
-  'recipe[ruby_build]',
-  'recipe[rbenv::user]',
   'recipe[users]',
   'recipe[workstation]',
   'recipe[mac_os_x::settings]',
   'recipe[mac_os_x::firewall]',
   'recipe[zip_app::data_bag]',
-  # Additional applications that have their own cookbooks
-  'recipe[oh-my-zsh-solo]',
-  'recipe[iterm2]',
-  'recipe[virtualbox]',
-  'recipe[1password]',
-  'recipe[xquartz]',
-  'recipe[wireshark]',
-  'recipe[vagrant]',
   # Roles
   'role[mac_os_x]'
 )
